01b1ea13a735f1d46495e939d495f7debf20fdab
Hermann Ritter <h.ritter1992@gmail.com>
2017-01-14 14:58:17 +0100

    Add files via upload

--- DSATUR.java
+++ DSATUR.java
@@ -2,6 +2,12 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 
+/*
+	1. STILL NOT EXACT FOR BIPARTITE GRAPHS - BUG NOT FOUND YET
+	2. PERFORMS WORSE FOR SOME GRAPHS THAN A CORRECTLY IMPLEMENTED DSATUR ALGORITHM
+	3. SPEED ALREADY QUITE GOOD
+*/
+
 public class DSATUR {
 	private int ub, lb, cn, vertices, edges;
 	private ColEdge[] graph;
@@ -21,9 +27,10 @@ public class DSATUR {
 		adjacencyMatrix = new int[vertices][vertices];
 		removed = new boolean[vertices];
 		adjacency();
-	    DSATUR();			
+	    DSATURmain();			
 	}
-
+	
+	//creates the adjacency matrix
 	public void adjacency() {
 		for (ColEdge edge : graph) {
 			adjacencyMatrix[edge.u-1][edge.v-1] = 1;
@@ -33,6 +40,7 @@ public class DSATUR {
 		adjSimple();
 	}
 	
+	//triangularizes the adjacency matrix
 	public void adjSimple() {
 		adjacencySimple = new int[vertices][vertices];
 		for (int i = 1; i < vertices; i++) {
@@ -42,7 +50,9 @@ public class DSATUR {
 		}
 	}
 	
-	public void DSATUR() {
+	//basically the same as with normal greedy coloring, only that the nodes aren't handled sequentially.
+	//but choseVertex() decides which node will be handled
+	public void DSATURmain() {
 		forbiddenColors = new int[vertices][vertices];
 		assignedColors = new int[vertices];
 		int tempK = 0;
@@ -52,7 +62,6 @@ public class DSATUR {
 		//is set for every row, which adjacent vertices have which color
 		boolean[] usedColors = new boolean[vertices];
 		usedColors[0] = true;
-		int nodeBefore = 0;
 		int vertex = 0;
 		
 		createDSATMatrix();
@@ -82,9 +91,6 @@ public class DSATUR {
 			usedColors[0] = true;
 			
 			removed[vertex] = true;
-			nodeBefore = vertex;
-			int index = 0;
-			//System.out.println(DSAT[vertex][3]);
 			updateDSATMatrix(vertex);
 		}
 		
@@ -103,35 +109,56 @@ public class DSATUR {
 	}
 	
 	public int choseVertex() {	
+		//sorts according to saturation
 		sort(1, DSAT);
+		
+		//this block looks for the vertex with the highest saturation which isn't already colored
 		int index = 0;
 		while (removed[DSAT[index][0]] == true) {
 			index++;
 		}
+		
+		//checks if the saturation value for the node chosen in the block before is the same
+		//for the next node, if yes, the next vertex will be chosen according to the highest
+		//degree of connectivity in that subset of vertices
 		if (index+1 < DSAT.length && DSAT[index][1] == DSAT[index+1][1]) {
 			int tempIndex = index;
 			
+			//WORKS (OKAYISH?)
+			
+			//looks for the start and end of the sub array which contains the vertices with the same
+			//degree of saturation
 			while (index+1 < DSAT.length && DSAT[index][1] == DSAT[index+1][1]) {
 				index++;
 			}
-			
 			int[][] subArray = new int[index-tempIndex+1][3];
 			
+			//copies this sub array into a new array
 			for (int i = 0; i < subArray.length; i++)
 				for (int j = 0; j < DSAT[0].length; j++) {
-					subArray[i][j] = DSAT[tempIndex][j];
-				}
+					subArray[i][j] = DSAT[i+tempIndex][j];
+				} 
 			
+			//sorts the sub array according to degrees of connectivity
 			sort(2, subArray);
+			
+			/*index = 0;
+			while (removed[subArray[index][0]]) {
+				index++;
+			}*/
+			
+			//returns the vertex with the highest degree of connectivity
 			return subArray[0][0];
 			
 		} else {
+			//if the vertex with the highest degree of saturation is the only vertex with that DSat, that vertex
+			//will be returned
 			return DSAT[index][0];
 		}
 	}
 		
 	
-	//GETESTET, FUNKTIONIERT
+	//sorts descending
 	public void sort(int column, int[][] array) {
 		Arrays.sort(array, new Comparator<int[]>() {
             @Override
@@ -141,18 +168,19 @@ public class DSATUR {
         });
 	}
 	
-	//SHOULD BE OK
+	//creates the DSAT matrix and sets the degrees of connectivity
 	public void createDSATMatrix() {
 		DSAT = new int[vertices][3];
 		for (int i = 0; i < vertices; i++) {
 			DSAT[i][0] = i;
 			for (int j = 0; j < vertices; j++) {
-				if (adjacencySimple[i][j] == 1)
+				if (adjacencyMatrix[i][j] == 1)
 					DSAT[i][2]++;
 			}
 		}
 	}
 	
+	//updates the degrees of saturation values (only for nodes adjacent to the one that was just colored)
 	public void updateDSATMatrix(int nodeBefore) {
 		for (int i = 0; i < vertices; i++) {
 			if (removed[i] == false && adjacencySimple[nodeBefore][i] == 1) {
@@ -167,6 +195,7 @@ public class DSATUR {
 		}
 	}
 	
+	//checks if the color with which an adjacent node was just colored is already a color of another adjacent node
 	public boolean checkIfColorIsAlreadyAdjacent(int index, int nodeBefore) {
 		for (int i = 0; i < vertices; i++) {
 			if (forbiddenColors[index][i] == assignedColors[nodeBefore] && i != nodeBefore)
@@ -175,6 +204,7 @@ public class DSATUR {
 		return true;
 	}
 	
+	//checks if the color the algorithm tries to color a node with is already used by an adjacent node
 	public boolean colorsBefore(int j, int row) {
 		for (int i = j; i >= 0; i--) {
 			if (forbiddenColors[row][i] == forbiddenColors[row][j])
